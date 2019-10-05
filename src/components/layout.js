762593f899212592e7e25ee41299e2a777d9f51c
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2019-10-05 14:12:13 +0200

    Cleanup all unused codes and assets

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,8 @@
 import React from "react";
-import Helmet from "react-helmet";
 
-import Header from "./header";
-import Footer from "./footer";
-import SEO from './seo'
-import "../css/style.css";
-
-const Layout = ({ className, headerClass, bodyClass, children, ...props }) => {
-  bodyClass = bodyClass || "container mx-auto px-6 md:px-10 lg:px-16";
+const Layout = () => {
   return (
-    <div className={className} {...props}>
-      <SEO/>
-      <Helmet>
-        <body className="font-sans antialiased" />
-      </Helmet>
-
-      <Header className={headerClass} />
-      <div className={bodyClass}>{children}</div>
-      <Footer />
+    <div>
     </div>
   );
 };
